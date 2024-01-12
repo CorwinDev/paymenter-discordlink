17360df5be0448b2d1d46067e0686781d44443cd
Corwin <info@corwindev.nl>
2024-01-12 18:41:42 +0100

    fix: fixed bug

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/linkedroles', [CorwinDev\PaymenterDiscordLink\Http\Controllers\LinkedRoleController::class, 'index'])->name('linkedroles.index')->middleware(['auth']);
-Route::get('/linkedroles/callback', [CorwinDev\PaymenterDiscordLink\Http\Controllers\LinkedRoleController::class, 'callback'])->name('linkedroles.callback')->middleware(['auth']);
+Route::get('/linkedroles', [CorwinDev\PaymenterDiscordLink\Http\Controllers\LinkedRoleController::class, 'index'])->name('linkedroles.index')->middleware(['auth', 'web']);
+Route::get('/linkedroles/callback', [CorwinDev\PaymenterDiscordLink\Http\Controllers\LinkedRoleController::class, 'callback'])->name('linkedroles.callback')->middleware(['auth', 'web']);
